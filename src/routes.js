a40cb90c1653e18c41e8f6bb5e85fa57fbdc07e6
Marco <>
2020-09-03 15:50:16 +0100

    root / post request now expects a trackName:{name} object and does a regex trackName search on the database and returns the results. Web Server first step!

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,24 @@ const router = express.Router();
 const blankModel = require("./models");
 
 router.post("/", async (req, res) => {
-  const result = await blankModel.find({});
-  res.send("Posted");
-  console.log(result);
+  const searchTerm = req.body.trackName;
+  if (!searchTerm) {
+    console.log("invalid request");
+    return;
+  }
+
+  const regex = new RegExp(searchTerm, "i");
+  console.log("Regex: " + regex);
+  const query = {
+    trackName: { $regex: regex },
+  };
+  blankModel
+    .find(query)
+    .lean()
+    .exec((err, doc) => {
+      const trackNames = doc.map((d) => d.trackName);
+      res.send(trackNames);
+    });
 });
 
 module.exports = router;

df918adcf0fc3ec2b2d73923509622d454fb6792
Marco <>
2020-10-03 19:08:56 +0100

    Update so server automatically pulls in the latest tags from Games_meta every 10 minutes, instead of just on startup.

--- src/GamesMeta.js
+++ src/GamesMeta.js
@@ -1,29 +1,40 @@
 const mongo = require("./Mongo");
+const { setIntervalAsync } = require("set-interval-async/dynamic");
+
+async function getTags(arrayOut) {
+  try {
+    console.log("Retreiving latest tags metadata...");
+    let query = { _id: "tags" };
+    const cursor = await mongo.collection_meta.find(query);
+    const hasResult = await cursor.hasNext();
+
+    if (!hasResult) {
+      return;
+    }
+
+    const result = await cursor.next();
+    console.log("Tags retrieved");
+    arrayOut.length = 0;
+    result.tags.map((tag) => {
+      arrayOut.push(tag);
+      return null;
+    });
+    console.log(arrayOut)
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 class GamesMeta {
-    constructor() {
-      this.tags = []
-    }
-  
-    async init() {
-      try {
-       
-        let query = { _id : "tags" } 
-        const cursor = await mongo.collection_meta.find(query);
-        const hasResult = await cursor.hasNext();
-    
-        if (!hasResult) {
-          return
-        }
-
-        const result = await cursor.next();
-        this.tags = [...result.tags]
-
-      } catch (err) {
-        console.log(err);
-      }
-    }
+  constructor() {
+    this.tags = [];
   }
-  
-  module.exports = new GamesMeta();
-  
+
+  async init() {
+    await getTags(this.tags);
+    const interval = 600000 // refresh every 10 mins
+    setIntervalAsync( getTags, interval, this.tags )
+  }
+}
+
+module.exports = new GamesMeta();

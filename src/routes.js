067081d8e4c0ab291409ead0b53730b1b347bb8f
Marco <>
2020-11-12 09:26:53 +0000

    Just fixing up the repo, after the submodule damage

--- src/routes.js
+++ src/routes.js
@@ -62,41 +62,54 @@ async function doSearch(searchTerm, count, offset, sortMethod, deviceFilter, pop
 
   // const query = { "lookupBlob.deviceFamilies": { $all: deviceFilter } }
 
-  let sortOptions = {
+  let options = {
     limit: count,
     skip: offset,
     sort: {
       [dbkeys.popularity]: -1,
     },
+    projection: {
+      trackName: 1,
+      [dbkeys.trackId] : 1,
+      [dbkeys.artworkURL] : 1,
+      [dbkeys.popularity] : 1,
+      [dbkeys.rating] : 1,
+      [dbkeys.releaseDate] : 1,
+      [dbkeys.formattedPrice] : 1,
+      [dbkeys.price] : 1,
+      [dbkeys.trackName] : 1,
+      [dbkeys.tags] : 1,
+      [dbkeys.metaRanking] : 1,
+    }
   };
 
   switch (sortMethod.toLowerCase()) {
     case "popularity":
-      sortOptions.sort = { [dbkeys.popularity]: -1 };
+      options.sort = { [dbkeys.popularity]: -1 };
       break;
 
     case "user rating":
-      sortOptions.sort = { "metaRanking": -1 };
+      options.sort = { "metaRanking": -1 };
       break;
 
     case "newest":
-      sortOptions.sort = { [dbkeys.releaseDate]: -1, "metaRanking": -1 };
+      options.sort = { [dbkeys.releaseDate]: -1, "metaRanking": -1 };
       break;
 
     case "oldest":
-      sortOptions.sort = { [dbkeys.releaseDate]: 1, "metaRanking": -1 };
+      options.sort = { [dbkeys.releaseDate]: 1, "metaRanking": -1 };
       break;
 
     case "price - lowest":
-      sortOptions.sort = { [dbkeys.price]: 1, "metaRanking": -1 };
+      options.sort = { [dbkeys.price]: 1, "metaRanking": -1 };
       break;
 
     case "price - highest":
-      sortOptions.sort = { [dbkeys.price]: -1, "metaRanking": -1 };
+      options.sort = { [dbkeys.price]: -1, "metaRanking": -1 };
       break;
 
     case "name":
-      sortOptions.sort = { [dbkeys.trackName]: 1 };
+      options.sort = { [dbkeys.trackName]: 1 };
       break;
 
     default:
@@ -105,11 +118,11 @@ async function doSearch(searchTerm, count, offset, sortMethod, deviceFilter, pop
 
   console.log("Request:");
   console.log(" - Query: " + JSON.stringify(query));
-  console.log(" - Sort: " + JSON.stringify(sortOptions.sort));
+  console.log(" - Sort: " + JSON.stringify(options.sort));
 
   // explain_find(query, sortOptions)
 
-  const cursor = await mongo.collection.find(query, sortOptions);
+  const cursor = await mongo.collection.find(query, options);
   const results = [];
   let resultsCount = 0;
 

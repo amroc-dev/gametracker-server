8c1a2622c1e64cff7e346e1e76f1cd4dcd860a13
Marco <>
2020-12-01 10:41:41 +0000

    Added production MemoryStore

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
-const mongo = require('./Mongo');
-const gamesMeta = require ('./GamesMeta');
+const cors = require("cors");
+const mongo = require("./Mongo");
+const gamesMeta = require("./GamesMeta");
 const routes = require("./routes");
 const slowDown = require("express-slow-down");
-const expressSession = require('express-session');
-const expressVisitorCounter = require('./visitor-counter');
+const expressSession = require("express-session");
+const MemoryStore = require("memorystore")(expressSession);
+const expressVisitorCounter = require("./visitor-counter");
 const usageCounters = require("./usage");
 
 const app = express();
@@ -15,19 +16,35 @@ const app = express();
 // speed limiter
 app.enable("trust proxy"); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
 const speedLimiter = slowDown({
-  windowMs: 4 * 60 * 1000, // 
-  delayAfter: 120, // 
+  windowMs: 4 * 60 * 1000, //
+  delayAfter: 120, //
   delayMs: 1250, // amount of ms delay to add per request over the limit (accumulates with every request)
-  maxDelayMs: 1250 // don't accumulate past this amount
+  maxDelayMs: 1250, // don't accumulate past this amount
 });
 app.use(speedLimiter);
 
 // status monitor
-app.use(require('express-status-monitor')( {path: '/__status'}));
+app.use(require("express-status-monitor")({ path: "/__status" }));
+
+app.use(
+  expressSession({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000, // prune expired entries every 24h
+    }),
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // visitor monitor
-app.use(expressSession({ secret: 'secret', resave: false, saveUninitialized: true }));
-app.use(expressVisitorCounter({ hook: counterId => usageCounters[counterId] = (usageCounters[counterId] || 0) + 1 }));
+app.use(
+  expressVisitorCounter({
+    hook: (counterId) =>
+      (usageCounters[counterId] = (usageCounters[counterId] || 0) + 1),
+  })
+);
 
 // body parser
 app.use(bodyParser.json());
